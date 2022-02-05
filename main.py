4c4765334ee6f6a4440f4cf5a385c8211e9012f5
jampinelo <jasonpineda100@gmail.com>
2022-02-05 14:32:48 -0500

    adding client creation and gitignore

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 
 
+clients='mateo,sofia,'
+
+def create_client(client_name):
+    global clients
+    
+    if client_name not in clients:
+        clients+=client_name
+        _add_comma()
+    else:
+        print('Client already exist')
+
+
+def list_clients():
+    global clients
+    print(clients)
+
+
+def _add_comma():
+    global clients
+    clients+=','
+
+
+def _print_welcome():
+    print('WELCOME TO YOUR NATILLERA')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
 
 if __name__=='__main__':
-    pass
+    _print_welcome()
+    command=input()
+
+    if command == 'C':
+        client_name=input('What is the client name?: ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
+
+
